0fdfb7ad15176c342821443dd147c0c0b4343d54
LongYinan <lynweklm@gmail.com>
2022-05-11 13:57:26 +0800

    Fix SWC dynamic transform with suspense but without ssr

---
+++ test/e2e/dynamic-with-suspense/index.test.ts
@@ -0,0 +1,51 @@
+import { createNext } from 'e2e-utils'
+import { NextInstance } from 'test/lib/next-modes/base'
+import { hasRedbox, renderViaHTTP } from 'next-test-utils'
+import webdriver from 'next-webdriver'
+
+describe('dynamic with suspense', () => {
+  let next: NextInstance
+
+  beforeAll(async () => {
+    next = await createNext({
+      files: {
+        'pages/index.js': `
+          import { Suspense } from "react";
+          import dynamic from "next/dynamic";
+          
+          const Thing = dynamic(() => import("./thing"), { ssr: false, suspense: true });
+          
+          export default function IndexPage() {
+            return (
+              <div>
+                <p>Next.js Example</p>
+                <Suspense fallback="Loading...">
+                  <Thing />
+                </Suspense>
+              </div>
+            );
+          }
+        `,
+        'pages/thing.js': `
+          export default function Thing() {
+            return "Thing";
+          }
+        `,
+      },
+      dependencies: {},
+    })
+  })
+  afterAll(() => next.destroy())
+
+  it('should render server-side', async () => {
+    const html = await renderViaHTTP(next.url, '/')
+    expect(html).toContain('Next.js Example')
+    expect(html).toContain('Thing')
+  })
+
+  it('should render client-side', async () => {
+    const browser = await webdriver(next.url, '/')
+    expect(await hasRedbox(browser)).toBe(false)
+    await browser.close()
+  })
+})
